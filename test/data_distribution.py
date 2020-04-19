da6c99adbe36306acd5fa682a1fab5e7e720f22f
Matthew <monkey545.jones@gmail.com>
2020-04-18 20:06:56 -0500

    Re-formatted functions

--- test/data_distribution.py
+++ test/data_distribution.py
@@ -46,16 +46,27 @@ class DataDistributor:
 
         return data
 
+    def parse_distribution(self, dist_name, desired_class):  
+        if dist_name == "Normal":
+            return distributions.Normal(desired_class, math.sqrt(self.classes))
 
-    def normal_distribution(self, desired_class, num_elements):
-        dist = distributions.Normal(desired_class, math.sqrt(self.classes))
+        else:
+            if desired_class == 1 or desired_class == 0:
+                rate = .9
+            else:
+                rate = 1 / desired_class
+
+            return distributions.Exponential(rate)
+
+    def distribute(self, dist, num_elements):
         data = []
 
         for _ in range(num_elements):
             label = self.classes
-            while(label >= self.classes):
 
+            while(label >= self.classes):
                 label = int(dist.sample().tolist())
+
                 if label < 0:
                     label *= -1
 
@@ -67,3 +78,7 @@ class DataDistributor:
             ##print("Index: ", index)
 
         return data
+
+    def change_distribution(self, dist_name, desired_class, num_elements):
+        dist = self.parse_distribution(dist_name, desired_class)
+        return self.distribute(dist, num_elements)
--- test/test_mnist_nonIID_distribution.py
+++ test/test_mnist_nonIID_distribution.py
@@ -11,8 +11,6 @@ def fetch_mnist_data():
     test = torch.utils.data.Subset(full_test, range(2000, 2500)) 
     return train, test
 
-## new system, input is "Geometric" or "Normal", then distribution is created based on a switch case. If all cases fail, pick normal distribution. 
-
 if __name__ == '__main__':
     train, test = fetch_mnist_data()
     dataDist = data_distribution.DataDistributor(train, 10)
@@ -20,8 +18,8 @@ if __name__ == '__main__':
     num_samples = 500
     skewed_class = 7
 
-    geom_data = dataDist.geometric_distribution(skewed_class, num_samples)
-    norm_data = dataDist.normal_distribution(skewed_class, num_samples)
+    geom_data = dataDist.change_distribution("Geometric", skewed_class, num_samples)
+    norm_data = dataDist.change_distribution("Normal", skewed_class, num_samples)
 
-    plt.hist([i[1] for i in norm_data])
+    plt.hist([i[1] for i in geom_data])
     plt.show()
