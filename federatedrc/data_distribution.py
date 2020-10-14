fccb29cc129a7eecff6193670c84af87e90c701b
Suhas <suhasraja1@gmail.com>
2020-10-13 19:21:44 -0500

    Additional documentation for sphinx

--- federatedrc/data_distribution.py
+++ federatedrc/data_distribution.py
@@ -3,15 +3,18 @@ import random
 
 ## Assumes data is not a tensor
 class DataDistributor:
+    ### Prestores data.
     def __init__(self, data, num_classes):
         self.classes = num_classes
         self.buckets = defaultdict(list)
         self.assign_data(data)
 
+    ### Collects data into according buckets based on precomputed bucket assignment
     def assign_data(self, data):
         for elem in data:
             self.buckets[elem[1]].append(elem)
 
+    ### Leverages Pythons random library to split data into non-iid subsets.
     def distribute_data(self, dist, num_elements):
         if sum(dist) > 1:
             dist = [float(a) / float(sum(dist)) for a in dist]
