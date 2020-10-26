b66a93689955cb174f08ed829a9e7e672cb84dac
matthew <matthew.mej@utexas.edu>
2020-10-25 22:19:20 -0500

    Added new configs

---
+++ tests/config/mnist_client_config3.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+import torch.optim as optim
+from federatedrc.client import ClientConfig
+
+client_config = ClientConfig(
+    server_ip = '192.168.1.100',
+    port = 8882,
+    model_file_name = 'mnist_sample_cnn_client3.pt',
+    training_history_file_name = 'train_history3.png',
+    tx_history_file_name = 'tx_history3.png',
+    local_epochs = 5,
+    episodes = 10,
+    batch_size = 1,
+    criterion = nn.CrossEntropyLoss(),
+    optimizer = optim.SGD,
+    optimizer_kwargs = {
+        'lr': 0.001,
+        'momentum': 0.9
+    },
+    parameter_threshold = 1e-4
+)
---
+++ tests/config/mnist_client_config4.py
@@ -0,0 +1,21 @@
+import torch.nn as nn
+import torch.optim as optim
+from federatedrc.client import ClientConfig
+
+client_config = ClientConfig(
+    server_ip = '192.168.1.100',
+    port = 8883,
+    model_file_name = 'mnist_sample_cnn_client4.pt',
+    training_history_file_name = 'train_history4.png',
+    tx_history_file_name = 'tx_history4.png',
+    local_epochs = 5,
+    episodes = 10,
+    batch_size = 1,
+    criterion = nn.CrossEntropyLoss(),
+    optimizer = optim.SGD,
+    optimizer_kwargs = {
+        'lr': 0.001,
+        'momentum': 0.9
+    },
+    parameter_threshold = 1e-4
+)
